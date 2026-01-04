502896d9e43f1ccdb48cd27ee9f5fb38c86ba171
Linus045 <linuswillms00@gmail.com>
2026-01-04 01:46:33 +0100

    fix: adds comments and fixes manga_cover_adder script

--- main.py
+++ main.py
@@ -7,124 +7,36 @@ from pathlib import Path
 # Value is a a list with the start and end chapter e.g. [0,9] will combine all chapters from 0 to 9
 # can be taken from sites like this: https://www.detectiveconanworld.com/wiki/Manga#Volume_21-30_-_Chapters_201-306
 mangaMapping = {
-    "1": [0,9],
-    "2": [10,19],
-    "3": [20,29],
-    "4": [30,39],
-    "5": [40,50],
-    "6": [51,60],
-    "7": [61,70],
-    "8": [71,80],
-    "9": [81,90],
-    "10": [91,100],
-    "11": [101,110],
-    "12": [111,120],
-    "13": [121,130],
-    "14": [131,140],
-    "15": [141,150],
-    "16": [151,160],
-    "17": [161,170],
-    "18": [171,180],
-    "19": [181,190],
-    "20": [191,200],
-    "21": [201, 211],
-    "22": [212, 221],
-    "23": [222, 231],
-    "24": [232, 242],
-    "25": [243, 253],
-    "26": [254, 263],
-    "27": [264, 273],
-    "28": [274, 284],
-    "29": [285, 295],
-    "30": [296, 306],
-    "31": [307, 317],
-    "32": [318, 328],
-    "33": [329, 339],
-    "34": [340, 350],
-    "35": [351, 361],
-    "36": [362, 372],
-    "37": [373, 382],
-    "38": [383, 392],
-    "39": [393, 403],
-    "40": [404, 413],
-    "41": [414, 424],
-    "42": [425, 435],
-    "43": [436, 446],
-    "44": [447, 457],
-    "45": [458, 468],
-    "46": [469, 479],
-    "47": [480, 490],
-    "48": [491, 500],
-    "49": [501, 510],
-    "50": [511, 521],
-    "51": [522, 532],
-    "52": [533, 543],
-    "53": [544, 553],
-    "54": [554, 564],
-    "55": [565, 575],
-    "56": [576, 586],
-    "57": [587, 597],
-    "58": [598, 608],
-    "59": [609, 619],
-    "60": [620, 630],
-    "61": [631, 641],
-    "62": [642, 652],
-    "63": [653, 663],
-    "64": [664, 674],
-    "65": [675, 685],
-    "66": [686, 696],
-    "67": [697, 707],
-    "68": [708, 718],
-    "69": [719, 729],
-    "70": [730, 740],
-    "71": [741, 751],
-    "72": [752, 762],
-    "73": [763, 773],
-    "74": [774, 784],
-    "75": [785, 795],
-    "76": [796, 806],
-    "77": [807, 817],
-    "78": [818, 828],
-    "79": [829, 839],
-    "80": [840, 850],
-    "81": [851, 861],
-    "82": [862, 872],
-    "83": [873, 882],
-    "84": [883, 893],
-    "85": [894, 904],
-    "86": [905, 915],
-    "87": [916, 926],
-    "88": [927, 937],
-    "89": [938, 948],
-    "90": [949, 959],
-    "91": [960, 970],
-    "92": [971, 981],
-    "93": [982, 992],
-    "94": [993, 1003],
-    "95": [1004, 1014],
-    "96": [1015, 1025],
-    "97": [1026, 1036],
-    "98": [1037, 1047],
-    "99": [1048, 1058],
-    "100": [1059, 1069],
-    "101": [1070, 1080],
-    "102": [1081, 1091],
-    "103": [1092, 1102],
-    "104": [1103, 1113],
-    "105": [1114, 1123],
-    "106": [1124, 1133]
+    "1": [190, 222],
 }
 
 mangaDir = Path('manga')
 tempDir = Path('temp')
 outputZips = Path('output')
 
-mangaName = "Case Closed - Fantranslation"
+mangaName = "Dandadan"
 
 pageNumber = 1
 
 onlyCreateZips = False
 
+
+# if true
+# directory structure should be:
+# <root>/manga/Dandadan.22 Ch.190/1.jpg
+# <root>/manga/Dandadan.22 Ch.190/2.jpg
+# ...
+
+# <root>/manga/Dandadan.22 Ch.191/1.jpg
+# <root>/manga/Dandadan.22 Ch.191/2.jpg
+# <root>/manga/Dandadan.22 Ch.191/3.jpg
+# ...
+# <root>/manga/Dandadan.22 Ch.192/1.jpg
+# ...
+# <root>/manga/Dandadan.23 Ch.193/1.jpg
+# ...
+# where 22 and 23 are the volume numbers and Ch.190,.. the chapter numbers
+# TODO: add description for the False case
 extractVolumeMappingFromDirectoryName = True
 
 
@@ -137,7 +49,7 @@ def main():
         volumePaths = dict()
         for chapter in sorted(mangaDir.iterdir(), key=lambda m: getChapterNumberFromChapterDirectoryName(m)):
             if chapter.is_dir():
-                splits = chapter.name.split(" ",2)
+                splits = chapter.name.split(" ", 2)
                 chapterNo = splits[1]
                 chapterName = ""
                 if len(splits) > 2:
@@ -164,6 +76,7 @@ def main():
 
 
 
+# Dandadan.01 Ch.123
 def getChapterNumberFromChapterDirectoryName(chapterDir):
     splits = chapterDir.name.split(' ')
     # Ch.0001 -> 1
@@ -171,7 +84,7 @@ def getChapterNumberFromChapterDirectoryName(chapterDir):
     return float(chapterNo)
 
 
-def findVolumeDirForChapter(filename,chapterNo):
+def findVolumeDirForChapter(filename, chapterNo):
     if extractVolumeMappingFromDirectoryName:
         # Vol.01 -> 1
         return filename.split(" ")[0].split(".")[1]
@@ -184,16 +97,16 @@ def findVolumeDirForChapter(filename,chapterNo):
     raise Exception("Volume for chapter {} not found".format(chapterNo))
 
 
-def copyChapterToOutputDir(chapterDir, volumeNo, outputDir): 
+def copyChapterToOutputDir(chapterDir, volumeNo, outputDir):
     global pageNumber
     for page in sorted(chapterDir.iterdir()):
         pageExt = page.suffix
         originalFilename = "{}_{:05d}".format(chapterDir.name, int(page.stem))
         outputFilename = ("{:05d} - Volume {} - {}".format(pageNumber, volumeNo, originalFilename))
-        outputFile = outputDir.joinpath("{}.{}".format(outputFilename,pageExt))
+        outputFile = outputDir.joinpath("{}.{}".format(outputFilename, pageExt))
         shutil.copy(page, outputFile)
         pageNumber = pageNumber + 1
 
 
 if __name__ == "__main__":
-   main()
+    main()
--- manga_cover_adder/main.py
+++ manga_cover_adder/main.py
@@ -1,14 +1,27 @@
 from pathlib import Path
 import zipfile
 
-coverDir = Path('/home/linus/Calibre Library/Gosho Aoyama/')
-volumesDir = Path('/home/linus/Calibre Library/Gosho Aoyama/')
+coverDir = Path('covers')
+volumesDir = Path('volumes')
 
 # name of the cover file that will be added inside the .cbz file
 # make sure its the first file in the archive
-coverFileName = '00000 - cover.jpg'
+coverFileNamePrefix = '00000 - cover - '
+
+
+# whether to grab the cover image from calibre or from the coverDir
+grabCoverFromCalibre = False
 
 def main():
+    if not coverDir.exists() or not volumesDir.exists():
+        coverDir.mkdir(exist_ok=True)
+        volumesDir.mkdir(exist_ok=True)
+        print(f'Please add the cover images to {coverDir} and the volumes to {volumesDir}')
+        print(f'The Cover image should be named "Volume 01 -... .jpg/png". Important is the volume number after the first space')
+        print(f"If you want to grab them from a calibre library, set 'grabCoverFromCalibre' to True. It will then use the cover.jpg file inside the volume directory")
+        print(f"The volumes inside the volumes directory should be the .cbz files, named like the cover files with the volume number after the first space")
+        return
+
     # iterate over the .cbz files, and add the cover image added via calibre 'cover.jpg'
     # The cbz files are located in the volumesDir directory inside
     # a sub directory with the name 'Case Closed - Volume 001 (...)'
@@ -20,59 +33,70 @@ def main():
 
     # iterate through the files, in this case they are all from the same author
     # so we can iterate through the whole directory
-    for volumeDir in volumesDir.iterdir():
-        # iterate through the files and find the cbz file
-        for volumeFile in volumeDir.iterdir():
-            if volumeFile.is_file() and volumeFile.suffix == '.cbz':
-                # add the cover image to the volume
+    if grabCoverFromCalibre:
+        for volumeDir in volumesDir.iterdir():
+            # iterate through the files and find the cbz file
+            for volumeFile in volumeDir.iterdir():
+                if volumeFile.is_file() and volumeFile.suffix == '.cbz':
+                    volumeNo = getVolumeNo(volumeDir)
+                    coverImage = getCoverfile(volumeDir, volumeNo)
+                    if coverImage is None:
+                        print(f"Cover image for volume {volumeDir} not found. Please make sure you added a cover image inside calibre")
+                        continue
 
-                coverImage = getCoverfile(volumeDir)
+                    # add the cover image to the volume
+                    print(f'Adding cover image {coverImage} to {volumeFile}')
+                    addCoverToVolume(volumeFile, coverImage)
+    else:
+        # iterate through the volume files directly
+        for volumeFile in volumesDir.iterdir():
+            if volumeFile.is_file() and volumeFile.suffix == '.cbz':
+                volumeNo = getVolumeNo(volumeFile)
+                coverImage = getCoverfile(volumeFile, volumeNo)
                 if coverImage is None:
-                    print(f"Cover image for volume {volumeDir} not found. Please make sure you added a cover image inside calibre")
+                    print(f"Cover image for volume {volumeNo} not found. Please make sure you added a cover image inside calibre")
                     continue
 
                 # add the cover image to the volume
                 print(f'Adding cover image {coverImage} to {volumeFile}')
                 addCoverToVolume(volumeFile, coverImage)
 
-def addCoverToVolume(volumeFile, coverImage):
+def addCoverToVolume(volumeFile: Path, coverImage: Path):
     # make it a zip file
     volumeFile = volumeFile.rename(volumeFile.with_suffix('.zip'))
 
+    coverFileName = coverFileNamePrefix + coverImage.stem + '.png'
     # add the cover image to the zip file
     with zipfile.ZipFile(volumeFile, 'a') as zf:
-        zf.write(coverImage, coverFileName)
+        # check if file already exists
+        if coverFileName in zf.namelist():
+            print(f'WARNING: Cover image {coverFileName} already exists in {volumeFile}')
+        else:
+            zf.write(coverImage, coverFileName)
 
     # rename back to cbz
     volumeFile = volumeFile.rename(volumeFile.with_suffix('.cbz'))
 
-def getCoverfile(volumedir):
+
+def getVolumeNo(volumeDir):
+    return int(volumeDir.stem.split(' ')[1])
+
+
+def getCoverfile(volumedir, volumeNo):
     #  find cover.jpg and return it
-    for file in volumedir.iterdir():
-        if file.is_file() and file.stem == 'cover':
-            return file
+    if grabCoverFromCalibre:
+        for file in volumedir.iterdir():
+            if file.is_file() and file.stem == 'cover':
+                return file
+    else:
+        # grab cover from coverDir instead of from calibre
+        for file in coverDir.iterdir():
+            # expected file namne: 'Volume 001 - cover.jpg'
+            if file.is_file() and int(file.stem.split(' ')[1]) == volumeNo:
+                return file
 
-
-    # TODO: add option to have a cover directory that contains all cover images instead of grabbing
-    # them from calibre
-    # foundVolumeDir = None
-    # for volumeDir in coverDir.iterdir():
-    #     if volumeDir.is_dir():
-    #         volumeDirNo = int(volumeDir.stem.split(' ')[1])
-    #         if volumeDirNo == volumeNo:
-    #             foundVolumeDir = volumeDir
-    #             break
-
-    # if foundVolumeDir is None:
-    #     print(f'Volume {volumeNo} not found')
-    #     return None
-
-    # for file in foundVolumeDir.iterdir():
-    #     if file.is_file() and file.stem == 'cover':
-    #         return file
-
-
-
+        print(f'Volume {volumeNo} not found')
+        return None
 
 if __name__ == '__main__':
     main()
